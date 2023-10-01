b6fc27203d0346bb04e4fc1cc0dc301cc482fac8
Jose Germán Martínez <jgermanmm@gmail.com>
2023-09-30 22:20:11 -0300

    cambio de texto en log de numero de puerto

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const { Character } = require("./src/database");
 const server = require("./src/server");
 
 server.listen(10000, () => {
-  console.log("Server listening on port 8004");
+  console.log("Server listening on port 10000");
 });
 
 Character();
